b5bd5ac6ebef4ad4b6d4fbe08f775c28862faf44
urbanvaperoom <33205428+urbanvaperoom@users.noreply.github.com>
2019-01-27 16:09:51 +0300

    implemented module view

--- js/calendar/calendar.js
+++ js/calendar/calendar.js
@@ -2,4 +2,23 @@ export default class Calendar {
   constructor() {
     
   }
+
+  static getDaysInMonth(year, month) {
+
+  }
+
+  getMonthDate(year, month) {
+    const daysInMonth = Calendar.getDaysInMonth(year, month);
+
+    return [
+      [undefined, undefined, 1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24, 25, 26],
+      [27, 28, 29, 30, undefined, undefined]
+    ];
+  }
+
 }
+
+Calendar.DAYS_IN_MONTH
--- js/calendar/calendar.view.js
+++ js/calendar/calendar.view.js
@@ -14,9 +14,40 @@ export default class CalendarView {
     this.tableHead = null;
     this.tableBody = null;
 
+    this.handleMonthSelectChange = this.handleMonthSelectChange.bind(this);
+    this.handleYearSelectChange = this.handleYearSelectChange.bind(this);
+    this.handlePrevMonthButtonClick = this.handlePrevMonthButtonClick.bind(this);
+    this.handleNextMonthButtonClick = this.handleNextMonthButtonClick.bind(this);
+
     this._init();
     this._render();
+    this._update();
   }
+
+  get year() {
+    return Number(this.yearSelect.value);
+  }
+
+  get month() {
+    return Number(this.monthSelect.value);
+  }
+
+  handleMonthSelectChange() {
+    this._update();
+  }
+
+  handleYearSelectChange() {
+    this._update();
+  }
+
+  handlePrevMonthButtonClick() {
+    this._update();
+  }
+
+  handleNextMonthButtonClick() {
+    this._update();
+  }
+
 
   _init() {
 
@@ -32,19 +63,19 @@ export default class CalendarView {
     );
 
     this.yearSelect = createElement('select', {
-        onchange: this.handleMonthSelectChange
+        onchange: this.handleYearSelectChange
       },
 
       //  Calendar.MONTH_NAMES
-      [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 ].map((year, index) => createElement('option', {
-        value: index,
-      }, year))
-
+      [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020].map(
+        (year, index) => createElement('option', {
+          value: index,
+        }, year))
     );
 
     this.prevMonthButton = createElement('button', {
       className: 'button',
-      onclick: this.handleNextMonthButtonClick
+      onclick: this.handlePrevMonthButtonClick
     }, '<')
 
     this.nextMonthButton = createElement('button', {
@@ -52,29 +83,55 @@ export default class CalendarView {
       onclick: this.handleNextMonthButtonClick
     }, '>')
 
-    this.tableHead = createElement('thead', null, 
-    ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map( weekday => createElement( 'th', null, weekday)))
+    this.tableHead = createElement('thead', null,
+      ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map(weekday => createElement('th', null, weekday)))
 
     this.tableBody = createElement('tbody', null);
 
-    this.table = createElement('table', { className: 'table is-bordered' }, this.tableHead, this.tableBody
-    )
+    this.table = createElement('table', {
+      className: 'table is-bordered'
+    }, this.tableHead, this.tableBody)
 
-    
+
   }
 
   _render() {
-    const element = createElement('div', { id: 'calendar' },
-      createElement( 'header', null,
+    const element = createElement('div', {
+        id: 'calendar'
+      },
+      createElement('header', null,
         this.prevMonthButton,
-        createElement('div', { className: 'select' }, this.monthSelect),
-        createElement('div', { className: 'select' }, this.yearSelect),
+        createElement('div', {
+          className: 'select'
+        }, this.monthSelect),
+        createElement('div', {
+          className: 'select'
+        }, this.yearSelect),
         this.nextMonthButton,
       ),
       this.table
     );
 
     this.rootElement.appendChild(element)
+  }
+
+  _update() {
+    const month = this.calendar.getMonthDate(this.year, this.month);
+
+    const tableBody = createElement(
+      'tbody',
+      null,
+      month.map(
+        week => createElement(
+          'tr',
+          null,
+          week.map(date => createElement('td', null, date))
+        )
+      )
+    );
+
+    this.table.replaceChild(tableBody, this.tableBody);
+    this.tableBody = tableBody;
   }
 
 }
